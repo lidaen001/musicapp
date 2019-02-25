e03c31e500cffdbc93f0362a2fdc549beaeefa15
LiDaen <lidaen@icloud.com>
2019-02-25 15:16:47 +0800

    增加tab组件

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
-// import router from './router'
+import router from './router'
 import fastclick from 'fastclick'
 
 import 'common/stylus/index.styl'
@@ -13,5 +13,6 @@ fastclick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
